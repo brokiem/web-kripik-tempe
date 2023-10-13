c255581c3bf3cc4d236f51f18a6c6092ef7c967c
brokiem <milyhqvideo@gmail.com>
2023-10-13 16:52:31 +0800

    Add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  base: 'web-kripik-tempe',
   plugins: [solid()],
   build: {
     target: 'esnext',
